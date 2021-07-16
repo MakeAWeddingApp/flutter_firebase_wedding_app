144af2379441567a1d081a0e90cfb41c17cddefe
MakeAWeddingApp <heathernickmiles@gmail.com>
2021-07-15 21:25:06 -0400

    initial local setup

--- android/app/src/main/kotlin/com/example/starter_architecture_flutter_firebase/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/starter_architecture_flutter_firebase/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.starter_architecture_flutter_firebase
+package com.makeaweddingapp.weddingapp1
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
